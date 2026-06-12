47d4c59cd94a2a96fbc493e725dade4263000c18
lheman N'GUESSAN <lhemannguessan@MacBook-Pro-Freewan.local>
2026-06-12 17:47:24 +0000

    Application Stateless : Modification couleur <Scaffold> : en vert et écriture : bonjour dans l'appBar.

---
+++ lib/my_home_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:calculator_app/my_home_page_state.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => MyHomePageState();
+}
